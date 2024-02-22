998b17c2cc62dcbfd80678ca4f15a45ec1d25d3b
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2024-02-22 14:56:28 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,7 +1,16 @@
 var EidProfile = "";
 
 $(document).ready(function () {
-  if(sessionStorage.getItem("EmpNumber_HR")==null) { location.href = "index.html"; }
+  //if(sessionStorage.getItem("EmpNumber_HR")==null) { location.href = "index.html"; }
+
+  var str = "";
+  var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
+  var sLineName = "Website";
+  var sLinePicture = "https://profile.line-scdn.net/0hoLlg-mNNMGNRHiaTpMdPNG1bPg4mMDYrKX8qVnIYOgYpe3QwbCp2AXVKaVN_fnMzOC16V3NMagF8";
+  sessionStorage.setItem("LineID", sLineID);
+  sessionStorage.setItem("LineName", sLineName);
+  sessionStorage.setItem("LinePicture", sLinePicture);
+
   Connect_DB();
   CheckData();
 });
@@ -19,8 +28,6 @@ function CheckData() {
       }
       alert("new");
       EidProfile = doc.id;
-      //str += '<div><img src="'+ doc.data().LinePicture +'" class="add-profile"></div>';
-      //str += '<div class="NameLine">'+ doc.data().LineName+'</div>';
       str1 += '<div><img src="'+ sessionStorage.getItem("LinePicture") +'" class="add-profile"></div>';
       str1 += '<div class="NameLine" style="color:#fff;">'+ doc.data().EmpName +'</div>';
       $("#MyProfile").html(str1);  
